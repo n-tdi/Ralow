666ab41e8642b60aba1a22e4295444c6f3069004
n-tdi <79174456+n-tdi@users.noreply.github.com>
2022-12-09 14:27:28 -0500

    rarrr

--- src/main/java/world/ntdi/ralow/Ralow.java
+++ src/main/java/world/ntdi/ralow/Ralow.java
@@ -1,7 +1,14 @@
 package world.ntdi.ralow;
 
+import java.util.Random;
+
 public final class Ralow {
     public String american(int age) {
         return (age > 10 ? "True blooded" : "not american");
+    }
+
+    public int age() {
+        Random r = new Random();
+        return r.nextInt(25);
     }
 }
